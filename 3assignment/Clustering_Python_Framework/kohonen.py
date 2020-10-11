c9862e4be81e91b79f84eeda08fb076939d39fd8
janseke10 <50013337+janseke10@users.noreply.github.com>
2020-10-11 21:10:59 +0200

    Add files via upload

--- 3assignment/Clustering_Python_Framework/kohonen.py
+++ 3assignment/Clustering_Python_Framework/kohonen.py
@@ -1,9 +1,13 @@
+"""kohonen.py"""
+import math
+import random
+
 class Cluster:
     """This class represents the clusters, it contains the
     prototype and a set with the ID's (which are Integer objects) 
     of the datapoints that are member of that cluster."""
     def __init__(self, dim):
-        self.prototype = [0.0 for _ in range(dim)]
+        self.prototype = [random.random() for _ in range(dim)]
         self.current_members = set()
 
 class Kohonen:
@@ -16,6 +20,7 @@ class Kohonen:
 
         # A 2-dimensional list of clusters. Size == N x N
         self.clusters = [[Cluster(dim) for _ in range(n)] for _ in range(n)]
+
         # Threshold above which the corresponding html is prefetched
         self.prefetch_threshold = 0.5
         self.initial_learning_rate = 0.8
@@ -23,7 +28,9 @@ class Kohonen:
         self.accuracy = 0
         self.hitrate = 0
 
+
     def train(self):
+        print(len(self.clusters[0][0].prototype))
         # Step 1: initialize map with random vectors (A good place to do this, is in the initialisation of the clusters)
         # Repeat 'epochs' times:
         #     Step 2: Calculate the square size and the learning rate, these decrease linearly with the number of epochs.
@@ -31,6 +38,46 @@ class Kohonen:
         #     For each vector its Best Matching Unit is found, and :
         #         Step 4: All nodes within the neighbourhood of the BMU are changed, you don't have to use distance relative learning.
         # Since training kohonen maps can take quite a while, presenting the user with a progress bar would be nice
+
+        ## do everything 'epochs times'
+        i = 0
+        while (i <= self.epochs):
+            i += 1
+            ## calculate size of the neighbourhood
+            r = (self.n/2)*(1 - i/self.epochs)
+            ## calculate learningrate
+            e = 0.8 * (1- i/self.epochs)
+            ##step3
+            for j, datapoint in enumerate(self.traindata):
+                ## calculate the distances to each prototype for each datapoint
+                distances = [[0.0 for _ in range(self.n)] for _ in range(self.n)]
+                for row in range(self.n):
+                    for col in range(self.n):
+                        clus = self.clusters[row][col]
+                        distance_squared = 0.0
+                        for k in range(self.dim):
+                            distance_squared += (datapoint[k] - clus.prototype[k]) ** 2
+                        ##add the euclidean distance to the 2d list
+                        distances[row][col] = math.sqrt(distance_squared)
+                print(distances)
+                ##find the BMU, by seeking the minimal distance
+                minimal = min([min(r) for r in distances])
+                BMU = self.clusters[0][0]
+                for row1 in range(self.n):
+                    for col1 in range(self.n):
+                        if minimal == distances[row1][col1]:
+                            BMU = self.clusters[row1][col1]
+                            break
+                for row2 in range(self.n):
+                    for col2 in range(self.n):
+                        curcol = self.clusters[row2][col2].prototype
+                        for i in range(self.n):
+                            ## TODO: this should have multiple if-statements, but i am not sure whether i am doing it correctly.
+                            ## It seems to me that you first need to make a list of nodes, and then change them. But also,
+                            ## i don't know if it is correct to change all the numbers in the vector.
+                            ## I'm afraid I did something wrong in the beginning :(
+                            if BMU.prototype[i] + r >= curcol[i]:
+                                self.clusters[row2][col2].prototype[i] = (1 - e) * self.clusters[row2][col2].prototype[i] + e * datapoint[i]
         pass
 
     def test(self):
